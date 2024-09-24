47b69c56573b6a46a8028dd7ae29f98219de7c81
Andrés Balladares Flores <168479277+BalladaresF@users.noreply.github.com>
2024-09-23 22:17:40 -0600

    Update Program.cs

--- AndresBalladares-Proyecto/Program.cs
+++ AndresBalladares-Proyecto/Program.cs
@@ -27,10 +27,10 @@ namespace Gestion.UI
 
 /*
  * Para que este proyecto se ejecute correctamente se necesita:
- *      - Tener la base de datos 0118520229 en el servidor. El script est· en el archivo Proyecto 3.
- *      - Asegurarse que el usuario y la contraseÒa escritos en API.appsettings.json y en Context
- *        coinciden con los del servidor al iniciar sesiÛn en SQL Server.
- *      - Asegurarse que baseurl en Services coincide con la direcciÛn del API, la cual est· en 
+ *      - Tener la base de datos 0118520229 en el servidor. 
+ *      - Asegurarse que el usuario y la contrase√±a escritos en API.appsettings.json y en Context
+ *        coinciden con los del servidor al iniciar sesi√≥n en SQL Server.
+ *      - Asegurarse que baseurl en Services coincide con la direcci√≥n del API, la cual est√° en 
  *        API.LaunchSettings.json.
  *        
  *       .NET utilizado: 7.0.
